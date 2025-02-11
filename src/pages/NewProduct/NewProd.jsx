2561296d4789934cdbd232a49e78174d314e88f9
Akbarxoja Joraxojayev <akbarjoraxojayev7@gmail.com>
2025-02-11 22:51:06 +0500

    logs patch

--- src/pages/NewProduct/NewProd.jsx
+++ src/pages/NewProduct/NewProd.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import './NewProd.scss'
 
 const NewProd = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <section className="newProd">
+        <div className="container">
+          <div className="newProd_wrapper">
+            <div className="newProd_content">
+              <h2>Новинки</h2>
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+    </>
+  );
+};
 
-export default NewProd
+export default NewProd;
--- src/pages/Sale/Sale.jsx
+++ src/pages/Sale/Sale.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import './Sale.scss'
 
 const Sale = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+          <section className='sale'>
+            <div className="container">
+                <div className="sale_wrapper">
+                    <div className="sale_content">
+                        <h2>Товары по акции</h2>
+                    </div>
+                </div>
+            </div>
+            </section>  
+        </>
     )
 }
 
